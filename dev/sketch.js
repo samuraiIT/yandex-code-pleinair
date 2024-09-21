9fde696fcd8c5ca92b7888e5f037b4c129b0099e
samurai <dir1020@yandex.ru>
2024-09-21 14:17:52 +0300

    Initial commit

---
+++ dev/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(450, 800);
+}
+
+function draw() {
+  background("white");
+
+  fill("green");
+  stroke("#10990050");
+  strokeWeight(20);
+
+  circle(width / 2, height / 2, 300);
+}
